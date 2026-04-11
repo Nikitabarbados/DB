8cb4360647c5a775aabdb78f235279080b01125b
ZemNyux <nikita.gaber2703@gmail.com>
2026-04-11 15:29:04 +0300

    Add files via upload

---
+++ ConsoleServer10/Client10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static async Task Main()
+    {
+        var client = new ClientNetwork();
+        await client.StartAsync();
+    }
+}
